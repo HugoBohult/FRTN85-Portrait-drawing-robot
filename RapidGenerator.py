39644a4fb593f7a5337dd655187165193c6811ee
HugoBohult <hugobohult@gmail.com>
2025-10-14 10:14:02 +0200

    Hej

--- RapidGenerator.py
+++ RapidGenerator.py
@@ -1,5 +1,3 @@
-#TO DO: Add real calibration points and drawing sequence
-
 #Plane finder sequence
 def plane_finder_sequence(file):
     file.write("\n\tPROC plane_finder()\n")
@@ -25,45 +23,50 @@ def define_Calibpoints(file):
     file.write("\tVAR robtarget planepoint2;\n")
     file.write("\tVAR robtarget planepoint3;\n\n")
 
-def define_Drawingpoints(file,pointlist):
-    for i, point in enumerate(pointlist):
-        name = f"Dpoint{i}"
-        file.write(stringify_point(name, point[0], point[1]))
-        
+def define_Drawingpoints(file,countours):
+    for i, cnt in enumerate(countours):
+        for j, point in enumerate(cnt):
+            name = f"Dpoint{i}_{j}"
+            file.write(stringify_point(name, point[0], point[1]))
+
 def stringify_point(name, x, y):
     return f"\tCONST robtarget {name}:=[[{x},{y},0],[1,0,0,0],[0,0,0,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n"
 
-def drawing_sequence(file, pointlist):
+def drawing_sequence(file, countours):
     file.write("\n\tPROC drawing_sequence()\n")
     file.write("\t\tMoveJ Dpoint0,v100,z1,BHH_pen\WObj:=Workobject_1;\n")
-    for i in range(len(pointlist)-1):
-        file.write(f"\t\tMoveL Dpoint{i+1},v100,z1,BHH_pen\WObj:=Workobject_1;\n")
+
+    for i, cnt in enumerate(countours):
+        file.write(f"\t\tMoveJ Dpoint{i}_{0},v100,z1,BHH_pen\WObj:=Workobject_1;\n")
+        for j, point in enumerate(cnt):
+            file.write(f"\t\tMoveL Dpoint{i}_{j},v100,z1,BHH_pen\WObj:=Workobject_1;\n")
+        file.write(f"\t\tMoveJ Dpoint{i}_{len(cnt)-1},v100,z1,BHH_pen\WObj:=Workobject_1;\n")
+
     file.write("\tENDPROC\n\n")
 
 
 
 #main program
-def main():
+def generator(countours):
     file = open("RAPID.txt", "w")
     file.write("MODULE Module1\n")
     file.write("\tCONST robtarget home:= [[280.027029643,125.4,-515.328184811],[0,0.608761442,0,0.79335333],[0,0,0,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n\n")
 
     #Runs the plane finder sequence
     define_Calibpoints(file)
-    define_Drawingpoints(file, [(20,20), (50,50), (30,50)])  # Example drawing points
+    define_Drawingpoints(file, countours)
     plane_finder_sequence(file)
-    drawing_sequence(file, [(20,20), (50,50), (30,50)])  # Example drawing points
+    drawing_sequence(file, countours)  # Example drawing points
 
     file.write("\tPROC main()\n")
     file.write("\t\tMoveJ home,v100,z1,BHH_pen\WObj:=Workobject_1;\n")
     file.write("\t\tplane_finder;\n")
     file.write("\t\tdrawing_sequence;\n")
 
-    #TO DO: Add drawing sequence here
-
     file.write("\t\tMoveJ home,v100,z1,BHH_pen\WObj:=Workobject_1;\n")
     file.write("\tENDPROC\n")
     file.write("ENDMODULE\n")
 
 if __name__ == "__main__":
-    main()
+    test_points = [(20, 20), (50, 50), (30, 50)]
+    main(test_points)
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello, World!")
+from RapidGenerator import generator
+from EdgeDraw import segment
+
+if __name__ == "__main__":
+    countours = segment("Edward.jpg")
+    generator(countours)
